bfaa0622f1e0faba8a04c3de02bb44708f8ecc1e
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2021-10-06 21:12:38 -0300

    Realizando ajustes ma classe de transferências

--- code/accounts/admin.py
+++ code/accounts/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Account, Permissions
+from .models import Account, Permissions, Translations
 
 # Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    fields = ("name", "permissions", "active")
 
 admin.site.register(Permissions)
-admin.site.register(Account)
+admin.site.register(Translations)
--- code/accounts/models.py
+++ code/accounts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+import random
+import string
+
 # Create your models here.
 class Permissions(models.Model):
     name = models.CharField(max_length=20, blank=False, null=False)
@@ -17,7 +20,7 @@ class Account(models.Model):
     name = models.CharField(max_length=20, blank=False, null=False)
     permissions = models.ForeignKey(Permissions, on_delete=models.PROTECT, blank=False, null=False)
     saldo = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False, default=0.0)
-    number = models.CharField(max_length=10, blank=False, null=False, unique=True)
+    number = models.CharField(max_length=10, blank=False, null=False, unique=True, auto_created=True)
     active = models.BooleanField(default=True)
 
     class Meta:
@@ -29,8 +32,16 @@ class Account(models.Model):
     def __str__(self) -> str:
         return self.name
 
+    def save(self, *args, **kwargs) -> None:
+        self.number = self.generate_number()
+        super().save(*args, **kwargs)
+
+    def generate_number(self, size=10) -> str:
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+
 class Translations(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.PROTECT, blank=False, null=False)
+    account = models.ForeignKey(Account, related_name="account_origin", on_delete=models.PROTECT, blank=False, null=False)
+    account_destino = models.ForeignKey(Account, related_name="account_destino", on_delete=models.PROTECT, blank=False, null=False)
     value = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
 
     class Meta:
